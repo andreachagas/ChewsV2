0f549c48117d66a3b8b7387c6df4c018ef3c2d58
Andrea <andrea.lima.chagas@gmail.com>
2015-04-16 15:53:54 +0800

    Chews version 2.0
    Cleaner and simpler version of previous Chews

---
+++ app/src/main/java/co/chagas/chewsv2/data/Constants.java
@@ -0,0 +1,17 @@
+package co.chagas.chewsv2.data;
+
+/**
+ * Created by andrealimachagas on 16/04/2015.
+ */
+public class Constants {
+    public static final String DATABASE_NAME = "datastorage";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "restaurant";
+    public static final String TITLE_NAME = "title";
+    public static final String TYPE_NAME = "type";
+    public static final String ADDRESS_NAME = "address";
+    public static final String PRICE_NAME = "price";
+    public static final String DRINK_NAME = "drink";
+    public static final String COMMENT_NAME = "comment";
+    public static final String KEY_ID = "_id";
+}
